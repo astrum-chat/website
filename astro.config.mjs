7668caedafb83cd9351c0c03f6618223e24f550b
Cameron Campbell <cameron.campbell325@gmail.com>
2026-02-05 06:23:09 +0000

    add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astrum-chat.github.io',
+  base: '/website',
+});
