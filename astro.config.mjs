d61879c48c890a1f91bad018014fb96c6688f4c3
Cameron Campbell <cameron.campbell325@gmail.com>
2026-02-05 06:36:57 +0000

    configure custom domain astrum.chat

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrum-chat.github.io',
-  base: '/website',
+  site: 'https://astrum.chat',
 });
